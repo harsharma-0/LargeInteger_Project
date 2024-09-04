c7ab5b7943e47a1bd992614cf737bf413da22158
Harsh Vishwakarma <harshsharma6341@gmail.com>
2024-09-04 12:50:44 +0530

    update

--- index.cpp
+++ index.cpp
@@ -193,3 +193,25 @@ LargeInt LargeInt::operator--(int temp){
     --(*this);
     return aux;
 }
+
+LargeInt &operator+=(LargeInt &a,const LargeInt& b){
+    int t = 0, s, i;
+    int n = length(a), m = length(b);
+    if(m > n){
+        a.digits.append(m - n, 0);
+    }
+    n = length(a);
+    for(i = 0; i< n;i++){
+        if(i < m){
+            s = (a.digits[i] + b.digits[i]) + t;
+        }else{
+            s = a.digits[i] + t;
+        }
+        t = s / 10;
+        a.digits[i] = s%10;
+    }
+    if(t){
+        a.digits.push_back(t);
+    }
+    return a;
+}
