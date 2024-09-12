17561a6a24ec0221eb6457eaf2bd06d16a1004d9
Harsh Vishwakarma <harshsharma6341@gmail.com>
2024-09-12 10:19:35 +0530

    update

--- index.cpp
+++ index.cpp
@@ -299,3 +299,37 @@ LargeInt operator-(const LargeInt &a, LargeInt &b){
     temp -= b;
     return temp;
 }
+
+LargeInt &operator*=(LargeInt &a, const LargeInt &b){
+    if(Null(a) || Null(b)){
+        a = LargeInt();
+        return a;
+    }
+    int n = a.digits.size(), m = b.digits.size();
+    vector<int> v(n+m,0);
+    for(int i = 0; i< n;i++){
+        for(int j = 0; j< m; j++){
+            v[i+j] += (a.digits[i]) * (b.digits[j]);
+        }
+    }
+    n += m;
+    a.digits.resize(v.size());
+    for(int s, i = 0, t = 0; i< n;i++){
+        s = t + v[i];
+        v[i] = s % 10;
+        t = s / 10;
+        a.digits[i] = v[i];
+    }
+    for(int i = n-1; i>=1 && !v[i]; i--){
+        a.digits.pop_back();
+    }
+    return a;
+}
+
+
+LargeInt operator*(const LargeInt&a, const LargeInt &b){
+    LargeInt temp;
+    temp = a;
+    temp *= b;
+    return temp;
+}
